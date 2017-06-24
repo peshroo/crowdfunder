a28a48ced722117e6d04ee1fbec8c9aa40d54e22
Ju-Hang Sin <juhangsin@gmail.com>
2017-06-24 15:53:48 -0400

    validation fail errors for projects, signup completed, still need to do ledge & login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			auto_login(@user)
+			flash[:notice] = "User has been created"
 			redirect_to projects_url
 		else
 			render 'new'
