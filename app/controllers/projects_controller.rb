ec3426e61920af25d67591018fc044f0634f29c1
Ju-Hang Sin <juhangsin@gmail.com>
2017-06-27 11:48:36 -0400

    owner name showing on a project, eror messages working

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,35 +1,35 @@
 class ProjectsController < ApplicationController
-  before_action :require_login, only: [:new, :create]
+	before_action :require_login, only: [:new, :create]
 
 
-  def index
-    @projects = Project.all
-    @projects = @projects.order(:end_date)
-  end
+	def index
+		@projects = Project.all
+		@projects = @projects.order(:end_date)
+	end
 
-  def show
-    @project = Project.find(params[:id])
-    @pledge = Pledge.where(project_id: params[:id])
-    @owner = @project
-  end
+	def show
+		@project = Project.find(params[:id])
+		@pledge = Pledge.where(project_id: params[:id])
+		@owner = @project.owner
+	end
 
-  def new
-    @project = Project.new
-    @project.rewards.build
-  end
+	def new
+		@project = Project.new
+		@project.rewards.build
+	end
 
-  def create
-    @project = Project.create(project_params)
-    @project.owner = current_user
-    
-      if @project.save
-      flash[:notice] = "Project Created!"
-      redirect_to projects_url
-    else
-      flash.now[:error] = "Sorry try again!"
-      render :new
-    end
-  end
+	def create
+		@project = Project.create(project_params)
+		@project.owner = current_user
+
+			if @project.save
+			flash[:notice] = "Project Created!"
+			redirect_to projects_url
+		else
+			flash.now[:error] = "Sorry try again!"
+			render :new
+		end
+	end
 
 #   def edit
 #   @product = Project.find(params[:id])
@@ -52,8 +52,8 @@ class ProjectsController < ApplicationController
 #     redirect_to "/products"
 #   end
 
-  private
-  def project_params
-    params.require(:project).permit(:title, :description, :goal, :start_date, :end_date, :image)
-  end
+	private
+	def project_params
+		params.require(:project).permit(:title, :description, :goal, :start_date, :end_date, :image)
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
-  authenticates_with_sorcery!
+	authenticates_with_sorcery!
 
-  validates :password, length: { minimum: 8 }, on: :create
-  validates :password, confirmation: true, on: :create
-  validates :password_confirmation, presence: true, on: :create
-  validates :email, uniqueness: true
+	has_many :projects
+	
+	validates :password, length: { minimum: 8 }, on: :create
+	validates :password, confirmation: true, on: :create
+	validates :password_confirmation, presence: true, on: :create
+	validates :email, uniqueness: true
 
-  ALLOWED_ROLES = ["user", "owner"]
+	ALLOWED_ROLES = ["user", "owner"]
 end
